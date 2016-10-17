390d6b0b8d6e5b14ad64bc57a353b14f9e33312a
Adam Finn Tulinius <atu@dbc.dk>
2016-10-17 14:29:04 +0200

    automatically run db migrations before start

--- docker/start-smaug.sh
+++ docker/start-smaug.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl -kfsSL "$SMAUG_CONFIG" -o /etc/smaug.json && PORT_OAUTH=8001 PORT_CONFIG=8002 PORT_ADMIN=8003 node /opt/smaug/src/main.js -f /etc/smaug.json
+set -euf
+
+curl -kfsSL "$SMAUG_CONFIG" -o /etc/smaug.json && cd /opt/smaug && npm run migrate && PORT_OAUTH=8001 PORT_CONFIG=8002 PORT_ADMIN=8003 node /opt/smaug/src/main.js -f /etc/smaug.json
